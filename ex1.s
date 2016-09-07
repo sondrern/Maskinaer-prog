ee3ba46d87bfcb1a0e916eb430fc05910ac39205
sondrern <sondrern@gmail.com>
2016-09-07 14:39:44 +0200

    Update ex1.s

--- ex1.s
+++ ex1.s
@@ -84,73 +84,46 @@
 _reset: 
 	//b .  // do nothing
 	
-ldr r1, cmu_base_addr
-	ldr r2, [r1, #CMU_HFPERCLKEN0]
-	mov r3, #1
-	lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
-	orr r2, r2, r3
-	str r2, [r1, #CMU_HFPERCLKEN0]
-	
-	cmu_base_addr:
-	.long CMU_BASE
+	ldr r1, =CMU_BASE
 
+	 //load current value of HFPERCLK ENBALE
+	 ldr r2, [r1, #CMU_HFPERCLKEN0]
 
+	 //Set bit for GPIO clk
+	 mov r3, #1 
+	 lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
+	 orr r2, r2, r3
 
-	ldr r1, gpio_pa_base_addr	
-	ldr r2, [r1, #GPIO_CTRL]
-	mov r3, #0x2
-	orr r2, r2, r3
+	 //Store new value 
+	 str r2, [r1, #CMU_HFPERCLKEN0]
+	 
+	 ldr r1, =GPIO_PA_BASE
+		
+	//set high drive strength
+	mov r2, #0x2
 	str r2, [r1, #GPIO_CTRL]
 	
-	gpio_pa_base_addr:
-	.long GPIO_PA_BASE
-
-
-
-
-	
-	ldr r2, [r1, #GPIO_MODEH]
-	mov r3, #0x55555555
-	orr r2, r2, r3
+	//set pins 8-15 output
+	ldr r2, =0x55555555
 	str r2, [r1, #GPIO_MODEH]
-
-
-
-	
-	ldr r2, [r1, #GPIO_DOUT]
-	mov r3,#1
-	lsl r3, r3, #9
-	orr r2, r2, r3
-	str r2, [r1, #GPIO_DOUT]
-	
-	
-	
-	ldr r1, gpio_pc_base_addr	
-	ldr r2, [r1, #GPIO_MODEL]
-	mov r3, #0x33333333
-	orr r2, r2, r3
+		
+	ldr r1, =GPIO_PC_BASE 
+ 	
+	//Set pins 0-7 to input
+	ldr r2, =0x33333333
 	str r2, [r1, #GPIO_MODEL]
 	
-	ldr r2, [r1, #GPIO_DOUT]
-	mov r3, #0xff
-	orr r2, r2, r3
+	//Enable interal pull-up
+	mov r2, #0xff
 	str r2, [r1, #GPIO_DOUT]
 	
-	
-	gpio_pc_base_addr:
-	.long GPIO_PC_BASE
-
-
-
-		ldr r1, =GPIO_PC_BASE
-		ldr r2, =GPIO_PA_BASE
+	ldr r1, =GPIO_PC_BASE
+	ldr r2, =GPIO_PA_BASE
 while: 	
-		ldr r3, [r1, #GPIO_DIN]
-		lsl r3, r3, #8
-		str r3, [r2, #GPIO_DOUT]
-		b while
-	
-	
+	ldr r3, [r1, #GPIO_DIN]
+	lsl r3, r3, #8
+	str r3, [r2, #GPIO_DOUT]
+	b while
 
 /////////////////////////////////////////////////////////////////////////////
 //
