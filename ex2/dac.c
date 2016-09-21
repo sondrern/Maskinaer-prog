ef4010b115b61244353d74f58baa4fae98be705b
martinSilverPC <martin_lesund@hotmail.com>
2016-09-21 11:01:22 +0200

    added simple setup

--- ex2/dac.c
+++ ex2/dac.c
@@ -13,4 +13,9 @@ void setupDAC()
 	   3. Enable left and right audio channels by writing 1 to DAC0_CH0CTRL and DAC0_CH1CTRL
 	   4. Write a continuous stream of samples to the DAC data registers, DAC0_CH0DATA and DAC0_CH1DATA, for example from a timer interrupt
 	 */
+
+	   *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_DAC0; //Enable Dac clock
+	   *DAC0_CTRL = 0x50010;					  // Prescale DAC clock
+	   *DAC0_CH0CTRL = 0x1;							//Enable left audio channel
+	   *DAC0_CH1CTRL = 0x1;							//Enable right audio channel
 }
