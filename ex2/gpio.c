c9c853c00b9811b34fa09d8e7f2de49c1af7881a
martinSilverPC <martin_lesund@hotmail.com>
2016-09-21 10:16:48 +0200

    La til enabling for div. ting

--- ex2/gpio.c
+++ ex2/gpio.c
@@ -6,13 +6,21 @@
 /* function to set up GPIO mode and interrupts*/
 void setupGPIO()
 {
-	/* TODO set input and output pins for the joystick */
+		/* TODO set input and output pins for the joystick */
 
-	/* Example of HW access from C code: turn on joystick LEDs D4-D8
-	   check efm32gg.h for other useful register definitions
-	 */
+	
 	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;	/* enable GPIO clock */
 	*GPIO_PA_CTRL = 2;	/* set high drive strength */
 	*GPIO_PA_MODEH = 0x55555555;	/* set pins A8-15 as output */
-	*GPIO_PA_DOUT = 0x0700;	/* turn on LEDs D4-D8 (LEDs are active low) */
+
+
+
+	*GPIO_PC_MODEL = 0x33333333; //sets pins c0 -7 as input
+	*GPIO_PC_DOUT = 0xff; // enable internal pullup
+
+
+	*GPIO_EXTIPSELL = 0x22222222; //Enable interrupts on port c (low register)
+	*GPIO_EXTIRISE = 0xff; //Rising edge trigger enabled
+	*GPIO_EXTIFALL = 0xff; //Falling edge trigger enabled
+	*GPIO_IEN = 0xff; //external interrupt enabled
 }
