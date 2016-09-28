da5955f77c0564709d4bb1a1e4aaec9ded85e108
martinSilverPC <martin_lesund@hotmail.com>
2016-09-28 08:31:11 +0200

    litt comments

--- ex2/timer.c
+++ ex2/timer.c
@@ -16,11 +16,11 @@ void setupTimer(uint16_t period)
 
 	   This will cause a timer interrupt to be generated every (period) cycles. Remember to configure the NVIC as well, otherwise the interrupt handler will not be invoked.
 	 */
-	   *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;
+	   *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;	//Enable clock
 	     
-	   *TIMER1_TOP = period;
-	   *TIMER1_IEN=0x1;
-	   *ISER0 |= 0xc; 
-	   *TIMER1_CMD=1;
+	   *TIMER1_TOP = period;		// Period to register
+	   *TIMER1_IEN=0x1; 		  //Enable timer interrupt generation
+	   *ISER0 |= 0xc;  			 // Enable timer interrupts
+	   *TIMER1_CMD=0x1; 		// Start the timer
 
 }
