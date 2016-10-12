c4e8f1e5cf04b5e779eb38a59acf53513a070086
jaredshowa <jareds@stud.ntnu.no>
2016-10-12 13:19:07 +0200

    Update timer.c

--- EX2NY/Polling/timer.c
+++ EX2NY/Polling/timer.c
@@ -3,26 +3,17 @@
 
 #include "efm32gg.h"
 
-/* function to setup the timer */
+
 void
 setupTimer (uint16_t period)
 {
-  /*
-     TODO enable and set up the timer
+ 
+  *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;	
 
-     1. Enable clock to timer by setting bit 6 in CMU_HFPERCLKEN0
-     2. Write the period to register TIMER1_TOP
-     3. Enable timer interrupt generation by writing 1 to TIMER1_IEN
-     4. Start the timer by writing 1 to TIMER1_CMD
-
-     This will cause a timer interrupt to be generated every (period) cycles. Remember to configure the NVIC as well, otherwise the interrupt handler will not be invoked.
-   */
-  *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;	//Enable clock
-
-  *TIMER1_TOP = period;		// Period to register
-  *TIMER1_IEN = 0x1;		//Enable timer interrupt generation
-  *ISER0 |= 0xc;		// Enable timer interrupts
-  *TIMER1_CMD = 0x1;		// Start the timer
+  *TIMER1_TOP = period;
+  *TIMER1_IEN = 0x1;
+  *ISER0 |= 0xc;	
+  *TIMER1_CMD = 0x1;
 
 }
 
