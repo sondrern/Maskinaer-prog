5c865812f5d2f3bbbf86f2cc5c3a93e1900b0ecb
sondrern <sondrern@gmail.com>
2016-11-02 14:50:03 +0100

    Update game.c

--- EX3/game.c
+++ EX3/game.c
@@ -6,6 +6,20 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello World, I'm game!\n");
+	frame_init();
 	testscreen();
+	
+	//////// GAMEPAD TEST ///////////////
+	
+	FILE *driver;
+	driver = fopen("/dev/gamepad", "rb");
+	while(1){
+		int *ppos = NULL;
+		//loff_t (*llseek) (driver, ppos, int);
+		char buf[0];
+		read_gamepad(driver, buf, 1, ppos);
+		printf("Buttons = %i \n", buf);
+		// DELAY !!!!!
+	}
 	exit(EXIT_SUCCESS);
 }
