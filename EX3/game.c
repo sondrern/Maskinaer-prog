c3b2bc342bd7fca6155a2ce4659b7207fb42b695
jaredshowa <jareds@stud.ntnu.no>
2016-11-02 11:09:30 +0100

    Update game.c

--- EX3/game.c
+++ EX3/game.c
@@ -1,20 +1,11 @@
+#include "frame.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "frame.h"
+
 
 int main(int argc, char *argv[])
 {
 	printf("Hello World, I'm game!\n");
-	initDisplay();
-//	draw_text(intro);
-//	refresh_screen();
-	
-	draw_rect(10, 10, 0xFFFF);
-	refresh_screen();
-	
-	
-	
-	
-	//return 0;
+	testscreen();
 	exit(EXIT_SUCCESS);
 }
