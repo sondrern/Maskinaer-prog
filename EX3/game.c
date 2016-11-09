650ffdf8cf6478e4af6f7c3e3c10e9d0ddac0ac0
sondrern <sondrern@gmail.com>
2016-11-09 09:31:33 +0100

    Update game.c

--- EX3/game.c
+++ EX3/game.c
@@ -1,13 +1,28 @@
-#include "frame.h"
+
+#ifndef _POSIX_C_SOURCE 
+#define _POSIX_C_SOURCE 1
+#define _GNU_SOURCE 1
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "frame.h"
+#include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <time.h>
+#include <math.h>
+#include <stdbool.h>
 
+#define bufferlen 8
+FILE *driver;
 
 int main(int argc, char *argv[])
 {
 	printf("Hello World, I'm game!\n");
-	init_game();
+	//init_game();
 	/*
 	while(1){
 		updategame();
@@ -16,22 +31,32 @@ int main(int argc, char *argv[])
 	*/
 	//////// GAMEPAD TEST ///////////////
 	
-	FILE *driver;
+	system("modprobe driver-gamepad");
 	driver = fopen("/dev/gamepad", "rb");
+
 	if(driver==NULL){
 		printf("dev/gamepad IS NOT A DIRECTORY\n");
 		exit(0);
 	}
 	while(1){
-		int *ppos = NULL;
-		//loff_t (*llseek) (driver, ppos, int);
-		char buf[0];
-		read_gamepad(driver, buf, 1, ppos);
-		printf("Buttons = %i \n", buf);
+	
+		int button = ((int)getc(driver));
+		printf(" Buttons = %i \n", button);
+		
+	/*
+		char buf[bufferlen];
+		getchar();			 
+	    printf("Reading from the device...\n");
+	    int ret = read(driver, buf, bufferlen);        // Read the response from the LKM
+	   if (ret < 0){
+		  perror("Failed to read the message from the device.");
+	   }
+	   printf("The received message is: [%s]\n", buf);
+		*/	   
+			   
 		usleep(1000000);
 	}
 	
 	
 	exit(EXIT_SUCCESS);
 }
-
